7d8010c47aa0a0b8a78b0d59a2373a4614519535
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2023-12-23 02:47:44 +0530

    updated client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,41 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MainPage from './components/MainPage'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Loader from './components/Loader'
+import axios from 'axios'
 
 const App = () => {
   const steganoRef = useRef(null)
+  const [initializing, setInitializing] = useState(true)
+
+  const initialize = async()=>{
+    try{
+      // const res = await axios.get("http://127.0.0.9:8080/")
+      const res = await axios.get("https://steganographer-flask-server.onrender.com/")
+      if(res){
+        // alert(res.data)
+        setInitializing(false)
+      }
+    }
+    catch(err){
+      console.log(err)
+      return
+    }
+  }
+
+  useEffect(()=>{
+    initialize()
+  },[])
+
   return (
     <div>
       <Navbar/>
       <Hero steganoRef={steganoRef}/>
       <MainPage steganoRef={steganoRef}/>
       <Footer/>
+      {initializing&&<Loader/>}
     </div>
   )
 }
